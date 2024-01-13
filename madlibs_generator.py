d4e21786400712b0e64c7a65d0a023773f432c3a
md-ahmodullah <contactme.ahmod@gmail.com>
2024-01-13 12:29:40 +0600

    Madlibs Generator Project Upload

---
+++ madlibs_generator.py
@@ -0,0 +1,29 @@
+with open('madlibs_story.txt', 'r') as file:
+    story = file.read()
+
+words = set()
+
+target_start = "<"
+target_end = ">"
+
+for i, char in enumerate(story):
+    if char == target_start:
+        start_of_word = i
+
+    if char == target_end:
+        word = story[start_of_word: i + 1]
+        words.add(word)
+
+print(words)
+
+answers = {}
+for word in words:
+    answer = input('Enter a word for '+word+': ')
+    answers[word] = answer
+
+print(answers)
+for word in words:
+    story = story.replace(word, answers[word])
+
+print(story)
+
